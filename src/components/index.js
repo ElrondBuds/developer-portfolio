653e7044d64cfaa1fccc543651982c1cf1937e75
ElrondBuds <elrond.buds@gmail.com>
2022-11-13 12:21:16 +0200

    up

--- src/components/index.js
+++ src/components/index.js
@@ -10,6 +10,5 @@ export { default as Experience } from'./Experience/Experience'
 export { default as Contacts } from'./Contacts/Contacts'
 export { default as Projects } from'./Projects/Projects'
 export { default as SingleProject } from './Projects/SingleProject/SingleProject'
-export { default as Achievement } from './Achievement/Achievement'
 
 export { default as BackToTop } from'./BackToTop/BackToTop'
