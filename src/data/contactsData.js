ca2d268253d64c525558c4ba726f0f7f28ea99f5
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-01-07 12:00:10 +0000

    update address

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,7 +3,7 @@
 export const contactsData = {
     email: 'ebuds.elrond@gmail.com',
     phone: '+40755308857',
-    address: 'Bucharest, Romania - 062067 ',
+    address: 'Distributed Worldwide ',
 
     sheetAPI: 'https://sheetdb.io/api/v1/996pikthxx27q'
 }
