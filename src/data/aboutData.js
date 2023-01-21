705339bbe09ade28837ce14bd0f49aa3a7b92388
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-01-21 11:28:35 +0000

    up

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -4,6 +4,6 @@ export const aboutData = {
     title: "eBudsDAO",
     description1: "eBuds is a fair and transparent NFT project, where everyone can join, contribute and take advantage of the benefits.",
     description2: "eBuds team members are not taking any fees, they are investing their time and money to build a strong community, with a long term vision.",
-    description3:  "eBuds is built on MultiversX and is a decentralized autonomous organization that is governed by the eBuds Community. It’s one of the first DAOs powered by @SUPERCIETY. The eBudsDAO manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
+    description3:  "eBuds is built on MultiversX and is a decentralized autonomous Community that is governed by the eBudsDAC. It’s one of the first DAOs powered by @SUPERCIETY. The eBudsDAC manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
     image: 2 ,
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,9 +2,9 @@ import resume from '../assets/pdf/Litepaper.pdf'
 import one from '../assets/svg/svguri/profile.png'
 
 export const headerData = {
-    name: 'eBudsDAO',
-    title: "Decentralized Autonomous Organization",
-    desciption:"In a world that is controlled by central authorities, eBudsDAO mission is to create a decentralized platform that is driven by the community and powered by the people.",
+    name: 'eBudsDAC',
+    title: "Decentralized Autonomous Community",
+    desciption:"In a world that is controlled by central authorities, eBudsDAC mission is to create a decentralized platform that is driven by the community and powered by the people.",
     image: one,
     resumePdf: resume
 }
