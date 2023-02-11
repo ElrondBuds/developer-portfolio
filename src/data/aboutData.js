1bcccf139e3d40db4412dae9a056cb15a017692b
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-02-11 15:54:15 +0000

    up

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -4,6 +4,6 @@ export const aboutData = {
     title: "eBudsDAC",
     description1: "eBuds is a fair and transparent NFT project, where everyone can join, contribute and take advantage of the benefits.",
     description2: "eBuds team members are not taking any fees, they are investing their time and money to build a strong community, with a long term vision.",
-    description3:  "eBuds is built on MultiversX and is a decentralized autonomous Community that is governed by the eBudsDAC. It’s one of the first DAOs powered by @SUPERCIETY. The eBudsDAC manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
+    description3:  "eBuds is built on MultiversX and is a decentralized autonomous Community that is governed by the eBudsDAC. The eBudsDAC manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
     image: 2 ,
 }
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/litepaperv2.pdf'
 import one from '../assets/svg/svguri/profile.png'
 
 export const headerData = {
-    name: 'eBudsDAC',
+    name: 'eBuds',
     title: "Decentralized Autonomous Community",
     desciption:"In a world that is controlled by central authorities, eBudsDAC mission is to create a decentralized platform that is driven by the community and powered by the people.",
     image: one,
