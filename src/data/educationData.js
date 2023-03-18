33ecdffe4fe5a170c98028aa4fa702b0679b99d7
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-03-18 02:29:18 +0000

    up

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,3 +1,7 @@
+import { ExploreRounded } from '@material-ui/icons';
+import { Linking } from 'react-native';
+
+const url="https://www.exrond.com"
 export const educationData = [
     {
         id: 1,
@@ -22,7 +26,7 @@ export const educationData = [
     },
     {
         id: 3,
-        institution: 'The EBUD token has been listed on  <a href="https://www.exrond.com/">exrond.com</a>. ',
+        institution: 'The EBUD token has been listed on  <Text onPress={() => Linking.openURL(url)}> {url}  </exrond.com>. ',
         course: 'We have initiated the development of our liquidity pool.',
         startYear: 'January',
         endYear: '2023'
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,3 +1,4 @@
+
 export const experienceData = [
     {
         id: 1,
