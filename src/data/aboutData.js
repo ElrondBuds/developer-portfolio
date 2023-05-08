27c6c8d09025d334712bc7bdafde05b6ddae3258
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-05-08 12:26:15 +0000

    up

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,8 +2,8 @@
 
 export const aboutData = {
     title: "eBudsDAC",
-    description1: "eBuds is a fair and transparent NFT project, where everyone can join, contribute and take advantage of the benefits.",
-    description2: "eBuds team members are not taking any fees, they are investing their time and money to build a strong community, with a long term vision.",
-    description3:  "eBuds is built on MultiversX and is a decentralized autonomous Community that is governed by the eBudsDAC. The eBudsDAC manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
+    description1: "The eBud project is a digital collection of Non-Fungible Tokens (NFTs) that serves multiple purposes. Firstly, it is used to build and grow a community of like-minded individuals who share a passion for Web3.0 technology. Secondly, the eBuds NFTs add value to the project by providing a secure and decentralized platform for running nodes on the MultiversX blockchain. is a fair and transparent NFT project, where everyone can join, contribute and take advantage of the benefits.",
+    description2: "Users can mint their own eBuds NFTs on Frameit.gg, and also stake them on Eneftor.com to earn $EBUD tokens. This incentivizes participation and engagement within the eBud ecosystem, while also increasing the overall value of the NFTs.eBuds team members are not taking any fees, they are investing their time and money to build a strong community, with a long term vision.",
+    description3:  "Finally, the $EBUD token is listed on multiple exchanges including Jexchange.io, Exrond.com, and Onedex.app, providing liquidity for the Web3.0 community. This makes it easier for people to trade and invest in the eBud project, and also contributes to the growth and adoption of Web3.0 technology overall.eBuds is built on MultiversX and is a decentralized autonomous Community that is governed by the eBudsDAC. The eBudsDAC manages the $EBUD Token, a cryptocurrency that is used to power the eBuds ecosystem.",
     image: 2 ,
 }
