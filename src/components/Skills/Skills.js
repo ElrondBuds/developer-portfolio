cda0642ce4e08f85c9de0656ddb4aa4687841344
eBudsDAO <106663150+ElrondBuds@users.noreply.github.com>
2023-05-08 12:23:58 +0000

    up

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -43,14 +43,7 @@ function Skills() {
                 </div>
         </div>
         
-         <p style={{color: theme.tertiary}}>
-            The eBud project is a digital collection of Non-Fungible Tokens (NFTs) that serves multiple purposes. Firstly, it is used to build and grow a community of like-minded individuals who share a passion for Web3.0 technology. Secondly, the eBuds NFTs add value to the project by providing a secure and decentralized platform for running nodes on the MultiversX blockchain.
-            Users can mint their own eBuds NFTs on Frameit.gg, and also stake them on Eneftor.com to earn $EBUD tokens. This incentivizes participation and engagement within the eBud ecosystem, while also increasing the overall value of the NFTs.
-            Finally, the $EBUD token is listed on multiple exchanges including Jexchange.io, Exrond.com, and Onedex.app, providing liquidity for the Web3.0 community. This makes it easier for people to trade and invest in the eBud project, and also contributes to the growth and adoption of Web3.0 technology overall.  
-                <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
-                    ❤
-                </span>
-            </p>
+
         </div>
     )
 }
